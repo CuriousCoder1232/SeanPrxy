5ac0075dd36d0cd41f89f7d596c62d923091ccb3
Sean <meyersean.c@gmail.com>
2019-01-06 17:31:06 -0600

    test PetInfo component

--- public/components/Petinfo.js
+++ public/components/Petinfo.js
@@ -1,26 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class PetImage extends React {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return <img src={this.props.image_url} className="pet-image" />;
-  }
-}
+const PetInfo = () => {
+  return (
+    <div>
+      <img src={this.props.image_url} className="pet-image" />
+      <div className="pet-description-container">
+        <div className="pet-description-header">Description</div>
+        <p className="pet-description">{this.props.description}</p>
+      </div>
+    </div>
+  );
+};
 
-export default PetImage;
-export default class PetDescription extends React.Component {
-  constructor(props) {
-      super(props)
-  }
-
-  render() {
-      return (
-          <div className="pet-description-container">
-              <div className='pet-description-header'>Description</div>
-              <p className='pet-description'>{this.props.description}</p>
-          </div>
-      )
-  }
-}
+export default PetInfo;
